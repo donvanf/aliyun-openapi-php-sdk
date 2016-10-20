8e08a8e0dbe87b0cdaec5788e9e8efb3dab6a084
donvanf <fudongfan@chemanman.com>
2016-10-20 13:22:38 +0800

    change config

--- aliyun-php-sdk-core/Config.php
+++ aliyun-php-sdk-core/Config.php
@@ -22,17 +22,19 @@ include_once 'Regions/EndpointConfig.php';
 
 //config sdk auto load path.
 Autoloader::addAutoloadPath("aliyun-php-sdk-ecs");
-Autoloader::addAutoloadPath("aliyun-php-sdk-batchcompute");
-Autoloader::addAutoloadPath("aliyun-php-sdk-sts");
-Autoloader::addAutoloadPath("aliyun-php-sdk-push");
-Autoloader::addAutoloadPath("aliyun-php-sdk-ram");
-Autoloader::addAutoloadPath("aliyun-php-sdk-ubsms");
-Autoloader::addAutoloadPath("aliyun-php-sdk-ubsms-inner");
-Autoloader::addAutoloadPath("aliyun-php-sdk-green");
-Autoloader::addAutoloadPath("aliyun-php-sdk-dm");
-Autoloader::addAutoloadPath("aliyun-php-sdk-iot");
+Autoloader::addAutoloadPath("aliyun-php-sdk-rds");
+//Autoloader::addAutoloadPath("aliyun-php-sdk-batchcompute");
+//Autoloader::addAutoloadPath("aliyun-php-sdk-sts");
+//Autoloader::addAutoloadPath("aliyun-php-sdk-push");
+//Autoloader::addAutoloadPath("aliyun-php-sdk-ram");
+//Autoloader::addAutoloadPath("aliyun-php-sdk-ubsms");
+//Autoloader::addAutoloadPath("aliyun-php-sdk-ubsms-inner");
+//Autoloader::addAutoloadPath("aliyun-php-sdk-green");
+//Autoloader::addAutoloadPath("aliyun-php-sdk-dm");
+//Autoloader::addAutoloadPath("aliyun-php-sdk-iot");
 
 //config http proxy	
-define('ENABLE_HTTP_PROXY', FALSE);
+define('ENABLE_HTTP_PROXY', false);
 define('HTTP_PROXY_IP', '127.0.0.1');
 define('HTTP_PROXY_PORT', '8888');
+
